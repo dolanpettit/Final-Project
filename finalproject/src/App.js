7db8356649ed2201857f8d6f502fb44d2fc064af
Dolan Pettit <dolanpettit@gmail.com>
2020-10-26 19:35:02 -0400

    Update: React app created and initial commit

---
+++ finalproject/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        ></img>
+      </div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
