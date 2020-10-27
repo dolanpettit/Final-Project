d467da25fe1dba1d89b3956009a9f23d4c33397e
Dolan Pettit <dolanpettit@gmail.com>
2020-10-26 20:01:40 -0400

    Update: Complete setup

---
+++ finalproject/src/Post.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Post() {
+  return (
+    <div class>
+      <h3>Username</h3>
+      <img
+        className="post__image"
+        src="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+        alt=""
+      ></img>
+      <h4>Username: Caption</h4>
+    </div>
+  );
+}
+
+export default Post;
