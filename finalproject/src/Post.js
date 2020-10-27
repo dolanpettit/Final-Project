02ec3c7f82d7ce6f37d15ac9d87eb9d11071450b
Dolan Pettit <dolanpettit@gmail.com>
2020-10-26 20:47:55 -0400

    Update: Started development branch, working on post functionality
    Signed-off-by: Dolan Pettit <dolanpettit@gmail.com>

--- finalproject/src/Post.js
+++ finalproject/src/Post.js
@@ -1,15 +1,26 @@
 import React from "react";
+import "./Post.css";
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
   return (
-    <div class>
-      <h3>Username</h3>
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt="DolanPettit"
+          src="/static/images/avatar/1.jpg"
+        ></Avatar>
+        <h3>Username</h3>
+      </div>
       <img
         className="post__image"
         src="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
         alt=""
       ></img>
-      <h4>Username: Caption</h4>
+      <h4 className="post__text">
+        <strong>dolanpettit</strong> My first post!
+      </h4>
     </div>
   );
 }
