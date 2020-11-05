ebe28bd506fe949379b5a5afdb93b035bd1b835d
Dolan Pettit <dolanpettit@gmail.com>
2020-11-04 20:33:39 -0500

    Update to deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const router = require("express").Router();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
@@ -9,6 +10,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function () {
