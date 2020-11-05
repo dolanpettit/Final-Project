c47a6a71231e508d98ed53b2ad128eee3e9ac5b9
Dolan Pettit <dolanpettit@gmail.com>
2020-11-04 20:26:48 -0500

    Deployment

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "public");
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static(publicPath));
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
-
-app.listen(port, () => {
-  console.log("Server running");
+// Start the API server
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
